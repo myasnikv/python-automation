25ab340586187f3b0187391399b34a2fda004234
Viktor Myasnikov <vmyasnikov@lohika.com>
2018-08-31 16:51:53 +0300

    Phase4 test

--- Phase4/utils/driver.py
+++ Phase4/utils/driver.py
@@ -6,6 +6,6 @@ def _driver():
     chrome_options = Options()
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome('./chromedriver/chromedriver', chrome_options=chrome_options)
+    driver = webdriver.Chrome('./Phase4/chromedriver/chromedriver', chrome_options=chrome_options)
     return driver
 
